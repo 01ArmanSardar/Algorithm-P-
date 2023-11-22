505a7073b86dfcf2d2060b001489c214e38c4413
ArmanSardar <mdarmansardar57@gmail.com>
2023-11-23 00:42:20 +0600

    pitron practice

---
+++ Week -01 --Introduction/Module 2.5 -week 01 Practice day 01/binary_search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int s=0;
+      int e=nums.size()-1;
+      while (s<=e)
+      {
+        int mid=s+(e-s)/2;
+        int x=nums[mid];
+        if (x==target)  return mid;
+        if (target<x) e=mid-1;
+        if (target>x) s=mid+1;
+
+      }  
+      return -1;
+    }
+};
