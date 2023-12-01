2db57fcb7ad66929745a93a4f18cff3090d5d117
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-01 21:26:40 +0600

    pitorn practice

--- week 02 --Graph Algorithm/Graph Traversal/Adjacency_list_implimantaion.cpp
+++ week 02 --Graph Algorithm/Graph Traversal/Adjacency_list_implimantaion.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 using namespace std;
 const int N =1e5+5;
 vector <int > adj[N];
@@ -10,7 +10,8 @@ int main()
     {
         int u,v;
         cin >>u>>v;
-        adj[u].push_back(v);
+        adj[u].push_back(v);//this singel line for directed graph
+        adj[v].push_back(u);//when previous line and this line is write for undirected graph,And their two line is unweihted edge.
     }
     for (int i=1;i<=n;i++)
     {
@@ -23,4 +24,34 @@ int main()
     }
     
     return 0;
+}*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+const int N =1e5+5;
+vector <pair<int, int> > adj[N];
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for (int i=0;i<m;i++)
+    {
+        int u,v,w;
+        cin >>u>>v>>w;
+        adj[u].push_back({v,w});//this singel line for directed graph
+       // adj[v].push_back({u,w});//when previous line and this line is write for undirected graph,And their two line is unweihted edge.
+    }
+    for (int i=1;i<=n;i++)
+    {
+        cout <<"List "<<i<<": ";
+        for (auto j:adj[i])
+        {
+            cout <<"("<< j.first<<" ," <<j.second<<")";
+        }
+        cout <<endl;
+    }
+    
+    return 0;
 }
+
