2563ee1a22cbb81be978fa0fd44b446d90cffe4e
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-08 19:16:24 +0600

    pitorn

---
+++ week 02 --Graph Algorithm/Module --8 - Assignment 02/distance.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e3 + 5;
+vector<int> adjlst[N];
+bool visited[N];
+int level[N];
+
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+    visited[s] = true;
+    level[s] = 0;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for (int v : adjlst[u])
+        {
+            if (visited[v] == true)
+                continue;
+
+            q.push(v);
+            visited[v] = true;
+            level[v] = level[u] + 1;
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+
+    for (int i = 0; i < e; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adjlst[u].push_back(v);
+        adjlst[v].push_back(u);
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int s, d;
+        cin >> s >> d;
+
+        // Reset arrays for each query
+        fill(visited, visited + N, false);
+        fill(level, level + N, 0);
+
+        bfs(s);
+
+        if (visited[d])
+        {
+            cout << level[d] << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
