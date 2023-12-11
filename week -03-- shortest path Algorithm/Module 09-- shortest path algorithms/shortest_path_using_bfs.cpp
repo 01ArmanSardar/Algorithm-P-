d2a989d0678e29d45160a51ac912f2c238555b92
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-11 20:16:47 +0600

    pitron practice

--- week -03-- shortest path Algorithm/Module 09-- shortest path algorithms/shortest_path_using_bfs.cpp
+++ week -03-- shortest path Algorithm/Module 09-- shortest path algorithms/shortest_path_using_bfs.cpp
@@ -3,23 +3,28 @@ using namespace std;
 const int N = 1e3 + 5;
 vector<int> adjlst[N];
 bool visited[N];
-int level [N];
+int level[N];
+int parent[N];
 void bfs(int s)
 {
     queue<int> q;
     q.push(s);
-    visited[s]=true;
-    level[s]=0;
+    visited[s] = true;
+    level[s] = 0;
+    parent[s] = -1;
+
     while (!q.empty())
     {
-        int u=q.front();
+        int u = q.front();
         q.pop();
-        for (int v:adjlst[u])
+        for (int v : adjlst[u])
         {
-            if (visited[v]==true) continue;
+            if (visited[v] == true)
+                continue;
             q.push(v);
-            visited[v]=true;
-            level[v]=level[u]+1;
+            visited[v] = true;
+            level[v] = level[u] + 1;
+            parent[v] = u;
         }
     }
 }
@@ -34,9 +39,26 @@ int main()
         adjlst[u].push_back(v);
         adjlst[v].push_back(u);
     }
-    int s,d;
-    cin>>s>>d;
+    int s, d;
+    cin >> s >> d;
     bfs(s);
-    cout <<level[d]<<endl;
+   cout << "distance : " << level[d] << endl;
+    /*for (int i = 1; i <= n; i++)
+    {
+        cout << "parent of : " << i << " :" << parent[i] << endl;
+    } */
+    vector<int> path;
+    int current = d;
+    while (current != -1)
+    {
+        path.push_back(current);
+        current = parent[current];
+    }
+    reverse(path.begin(), path.end());
+    cout << "path : ";
+    for (int node : path)
+    {
+        cout << node << " ";
+    }
     return 0;
 }
