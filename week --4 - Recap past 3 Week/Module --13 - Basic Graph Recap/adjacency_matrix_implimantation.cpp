af6c24902c0f5ad22c3ba88b76839853da1e5b30
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-16 21:29:30 +0600

    pitron practice

---
+++ week --4 - Recap past 3 Week/Module --13 - Basic Graph Recap/adjacency_matrix_implimantation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adjmt[n + 1][n + 1];
+    for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            adjmt[i][j] = 0;
+        }
+    }
+   /*  for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+           cout << adjmt[i][j]<<" ";
+        }
+        cout <<endl;
+    } */
+
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adjmt[a][b] = 1;
+       // adjmt[b][a] = 1;
+    }
+
+     for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <=n; j++)
+        {
+           cout << adjmt[i][j]<<" ";
+        }
+        cout <<endl;
+    }
+    return 0;
+}
