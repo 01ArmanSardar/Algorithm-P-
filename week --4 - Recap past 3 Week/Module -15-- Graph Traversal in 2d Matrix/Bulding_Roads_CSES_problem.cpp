22b0d51493f95e9cd0a13a6ca9068b9600136674
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-19 12:40:31 +0600

    pitron practice

--- week --4 - Recap past 3 Week/Module -15-- Graph Traversal in 2d Matrix/Bulding_Roads_CSES_problem.cpp
+++ week --4 - Recap past 3 Week/Module -15-- Graph Traversal in 2d Matrix/Bulding_Roads_CSES_problem.cpp
@@ -32,9 +32,10 @@ int main()
             dfs(i);
         }
     }
-    for (int i=0;i<ans.size();i++)
+    cout << ans.size() - 1 << endl;
+    for (int i = 0; i < ans.size()-1; i++)
     {
-        cout << ans[i]<<endl;
+        cout << ans[i] <<" "<< ans[i + 1] << endl;
     }
 
     return 0;
