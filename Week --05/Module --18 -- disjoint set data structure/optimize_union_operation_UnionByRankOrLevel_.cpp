b77fb19dbdebfd4c6bc734e213b5081a5cdae3b6
ArmanSardar <mdarmansardar57@gmail.com>
2023-12-26 15:52:08 +0600

    pitorn practice

--- Week --05/Module --18 -- disjoint set data structure/optimize_union_operation_UnionByRankOrLevel_.cpp
+++ Week --05/Module --18 -- disjoint set data structure/optimize_union_operation_UnionByRankOrLevel_.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int parent[1000];
+int parent_level[1000];
 void dsu_set(int n)
 {
     for (int i = 1; i <= n; i++)
     {
         parent[i] = -1;
+        parent_level[i] = 0;
     }
 }
 int dsu_find(int node)
@@ -22,7 +24,20 @@ void dsu_union(int a, int b)
     int leaderB = dsu_find(b);
     if (leaderA != leaderB)
     {
-        parent[b] = a;
+
+        if (parent_level[leaderA] > parent_level[leaderB])
+        {
+            parent[leaderB] = leaderA;
+        }
+        else if (parent_level[leaderA] < parent_level[leaderB])
+        {
+            parent[leaderA] = leaderB;
+        }
+        else
+        {
+            parent[leaderB] = leaderA;
+            parent_level[leaderA]++;
+        }
     }
 }
 int main()
@@ -36,6 +51,6 @@ int main()
         cin >> a >> b;
         dsu_union(a, b);
     }
-cout <<dsu_find(5);
+    dsu_find(5);
     return 0;
 }
--- Week --05/Module --18 -- disjoint set data structure/optimize_union_operation_Union_By_Size_implimantation.cpp
+++ Week --05/Module --18 -- disjoint set data structure/optimize_union_operation_Union_By_Size_implimantation.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 int parent[1000];
+int parent_size[1000];
 void dsu_set(int n)
 {
     for (int i = 1; i <= n; i++)
     {
         parent[i] = -1;
+        parent_size[i] = 1;
     }
 }
 int dsu_find(int node)
@@ -20,9 +22,15 @@ void dsu_union(int a, int b)
 {
     int leaderA = dsu_find(a);
     int leaderB = dsu_find(b);
-    if (leaderA != leaderB)
+    if (parent_size[leaderA] > parent_size[leaderB])
     {
-        parent[b] = a;
+        parent[leaderB] = leaderA;
+        parent_size[leaderA] += parent_size[leaderB];
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        parent_size[leaderB] += parent_size[leaderA];
     }
 }
 int main()
@@ -36,6 +44,6 @@ int main()
         cin >> a >> b;
         dsu_union(a, b);
     }
-cout <<dsu_find(5);
+dsu_find(5);
     return 0;
 }
