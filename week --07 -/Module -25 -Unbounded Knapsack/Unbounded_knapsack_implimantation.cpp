339447c3906d80ec00ea0c46d1ee168e1af336c5
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-03 00:46:00 +0600

    pitron practice

--- week --07 -/Module -25 -Unbounded Knapsack/Unbounded_knapsack_implimantation.cpp
+++ week --07 -/Module -25 -Unbounded Knapsack/Unbounded_knapsack_implimantation.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[1005][1005];
 int unbounded_knapsack(int n, int s, int val[], int wgt[])
 {
     if (n == 0 || s == 0)
         return 0;
+    if (dp[n][s] != -1)
+        return dp[n][s];
     if (wgt[n - 1] <= s)
     {
         int ch1 = val[n - 1] + unbounded_knapsack(n, s - wgt[n - 1], val, wgt); // nie dekbo
         int ch2 = unbounded_knapsack(n - 1, s, val, wgt);
-        return max(ch1, ch2);
+        return dp[n][s] = max(ch1, ch2);
     }
     else
-        unbounded_knapsack(n - 1, s, val, wgt);
+       return dp[n][s]= unbounded_knapsack(n - 1, s, val, wgt);
 }
 int main()
 {
@@ -27,6 +30,13 @@ int main()
     {
         cin >> wgt[i];
     }
-cout <<unbounded_knapsack(n,s,val,wgt)<<endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+    cout << unbounded_knapsack(n, s, val, wgt) << endl;
     return 0;
 }
---
+++ week --07 -/Module -25 -Unbounded Knapsack/unbounded_knapsack_bottom_up_approch.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int val[n];
+    int wgt[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> wgt[i];
+    }
+    int dp[n + 1][s + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            if (wgt[i - 1] <= j)
+            {
+                dp[i][j] = max(val[i - 1] + dp[i][j - wgt[i - 1]], dp[i - 1][j]);
+            }
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    cout <<dp[n][s]<<endl;
+    return 0;
+}
