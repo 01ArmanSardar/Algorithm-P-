a7a2a365b4c1da79c60d2410cba7865dae4f6499
ArmanSardar <mdarmansardar57@gmail.com>
2024-01-04 20:17:27 +0600

    pitron practice

--- week --07 -/Module --26 --Longest Common subsequence/Lcs_top_down_approceh.cpp
+++ week --07 -/Module --26 --Longest Common subsequence/Lcs_top_down_approceh.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[1005][1005] = {-1};
 int lcs(string a, int n, string b, int m)
 {
-    if (n==0 ||m==0) return 0;
+    if (n == 0 || m == 0)
+        return 0;
+    if (dp[n][m] != -1)
+        return dp[n][m];
     if (a[n - 1] == b[m - 1])
     {
         int ans = lcs(a, n - 1, b, m - 1);
-        return ans + 1;
+        return dp[n][m] = ans + 1;
     }
     else
     {
         int ans = lcs(a, n - 1, b, m);
         int ans2 = lcs(a, n, b, m - 1);
-        return max(ans, ans2);
+        return dp[n][m] = max(ans, ans2);
     }
 }
 int main()
 {
     string a, b;
     cin >> a >> b;
-    cout << lcs(a, a.size(), b,b.size()) << endl;
+    memset(dp,-1,sizeof(dp));//memset (array name,array the je value set korbho,array er size dibho)
+    cout << lcs(a, a.size(), b, b.size()) << endl;
     return 0;
 }
